2073d621534759a401663ff145f9162c16ed5e77
TheMiA <t3h.designer@live.com>
2011-09-16 02:02:23 -0400

    Second fetch attempt.

--- LearningCurve/src/me/baufive/learningcurve/LearningCurve.java
+++ LearningCurve/src/me/baufive/learningcurve/LearningCurve.java
@@ -5,6 +5,6 @@ public class LearningCurve {
 		System.out.println("First example to Verean!");
 		System.out.println("Second commit!");
 		System.out.println("Hello!");
-		System.out.println("Verean Pull.");
+		System.out.println("Verean fetch.");
 	}
 }
