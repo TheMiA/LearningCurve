e741a7cc605bba654d7d7e156c82aa5409f96f15
Verean <verean@gmail.com>
2011-09-16 20:42:59 -0600

    Push

--- LearningCurve/src/me/baufive/learningcurve/LearningCurve.java
+++ LearningCurve/src/me/baufive/learningcurve/LearningCurve.java
@@ -5,6 +5,6 @@ public class LearningCurve {
 		System.out.println("First example to Verean!");
 		System.out.println("Second commit!");
 		System.out.println("Hello!");
-		System.out.println("Verean fech.");
+		System.out.println("Verean fecth.");
 	}
 }
